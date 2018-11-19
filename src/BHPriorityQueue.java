d5fa00acbccfdb1635260bb8c6a9258404c8924f
dan2dev <ccastro.dan@gmail.com>
2018-11-19 13:16:03 -0800

    Add BHPriorityQueue

--- src/BHPriorityQueue.java
+++ src/BHPriorityQueue.java
@@ -1,11 +1,11 @@
 import java.util.LinkedList;
 
-public class BHPriorityQueue<K extends Comparable, V>  implements VCPriorityQueue<K, V> {
+public class BHPriorityQueue<K extends Comparable, V> implements VCPriorityQueue<K, V> {
 
     private LinkedList<Entry<K, V>> queue;
 
     public BHPriorityQueue() {
-        queue = new LinkedList<>();
+        queue = new LinkedList<Entry<K, V>>();
     }
 
     @Override
@@ -20,22 +20,43 @@ public class BHPriorityQueue<K extends Comparable, V>  implements VCPriorityQueu
 
     @Override
     public Entry<K, V> enqueue(K key, V value) throws IllegalArgumentException {
-
-        return null;
+        Entry<K, V> entry = new Entry<>(key, value);
+        queue.add(entry);
+        int newIndex = this.sortHeap();
+        return this.queue.get(newIndex);
     }
 
     @Override
     public Entry<K, V> peek() {
-        return null;
+        return this.queue.get(0);
     }
 
     @Override
     public Entry<K, V> dequeueMin() {
-        return null;
+        Entry r = this.queue.pop();
+        this.sortHeap();
+        return r;
     }
 
     @Override
     public VCPriorityQueue<K, V> merge(VCPriorityQueue<K, V> other) {
-        return null;
+        ALPriorityQueue<K, V> otherQ = ((ALPriorityQueue) other);
+        for (Entry<K, V> entry : otherQ.getQueue()) {
+            this.queue.add(entry);
+        }
+        return this;
+    }
+
+    private int sortHeap() {
+        Integer index = this.queue.size() - 1;
+        Integer parentIndex = (int) Math.floor(index / 2.0);
+        while (this.queue.get(index) != null && this.queue.get(index).getKey().compareTo(this.queue.get(parentIndex).getKey()) < 0) {
+            Entry temp = this.queue.get(parentIndex);
+            this.queue.set(parentIndex, this.queue.get(index)); // do the swap
+            this.queue.set(index, temp);
+            index = parentIndex;
+            parentIndex = (int) Math.floor(index / 2.0);
+        }
+        return index;
     }
 }
--- src/UnitTest.java
+++ src/UnitTest.java
@@ -22,6 +22,13 @@ public class UnitTest {
         queueInstance.enqueue(3, "Man sneezing");
         queueInstance.enqueue(1,"Man Dying");
         queueInstance.enqueue(2, "Man cut his finger");
+        queueInstance.enqueue(4, "Mr Nobody 1");
+        queueInstance.enqueue(5, "Mr Nobody 2");
+        queueInstance.enqueue(6, "Mr Nobody 6");
+        queueInstance.enqueue(7, "Mr Nobody 7");
+        queueInstance.enqueue(8, "Mr Nobody 8");
+        queueInstance.enqueue(9, "Mr Nobody 9");
+        queueInstance.enqueue(10, "Mr Nobody 10");
 
         assertEquals("Man Dying", queueInstance.dequeueMin().getValue());
         assertEquals("Man cut his finger", queueInstance.dequeueMin().getValue());
