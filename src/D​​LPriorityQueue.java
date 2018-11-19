2737ee75aadd8dc65cdad5413ff4c88ccb124888
Shota Iwamoto <amaginqoo@gmail.com>
2018-11-19 10:15:47 -0800

    Add DLPriorityQueue

--- src/D​​LPriorityQueue.java
+++ src/D​​LPriorityQueue.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class D​​LPriorityQueue<K extends Comparable, V>  implements VCPriorityQueue<K, V> {
@@ -6,7 +6,7 @@ public class D​​LPriorityQueue<K extends Comparable, V>  implements VCPriori
     private List<Entry<K, V>> queue;
 
     public D​​LPriorityQueue() {
-       queue = new ArrayList<>();
+       queue = new LinkedList<>();
     }
 
     @Override
@@ -21,21 +21,43 @@ public class D​​LPriorityQueue<K extends Comparable, V>  implements VCPriori
 
     @Override
     public Entry<K, V> enqueue(K key, V value) throws IllegalArgumentException {
-        return null;
+        Entry<K, V> newEntry = new Entry<>(key, value);
+        Entry<K, V> minEntry = queue.get(0);
+        int minIndex = 0;
+        for (int i = 0; i < queue.size(); i++){
+             if (queue.get(i).getKey().compareTo(minEntry.getKey()) < 0){
+                 minEntry = queue.get(i);
+                 minIndex = i;
+             }
+        }
+        queue.add(minIndex, newEntry);
+        return newEntry;
     }
 
     @Override
     public Entry<K, V> peek() {
-        return null;
+        return queue.get(0);
     }
 
     @Override
     public Entry<K, V> dequeueMin() {
-        return null;
+        return queue.remove(0);
     }
+
+    public List<Entry<K, V>> getQueue() { return queue;}
 
     @Override
     public VCPriorityQueue<K, V> merge(VCPriorityQueue<K, V> other) {
-        return null;
+        D​​LPriorityQueue<K, V> otherQ = ((D​​LPriorityQueue) other);
+        for (int i = 0; i < queue.size(); i++) {
+            for (int j = 0; j < otherQ.size(); j++){
+                if (otherQ.getQueue().get(i).getKey().compareTo(queue.get(j).getKey()) < 0) {
+                    this.queue.add(j, otherQ.getQueue().get(i));
+                } else {
+                    this.queue.add(0, otherQ.getQueue().get(i));
+                }
+            }
+        }
+        return this;
     }
 }
